a629763eec41ae6a1e36baa39deeda12ed63a04d
seokjun <ksh730506@naver.com>
2025-04-01 20:11:33 +0900

    a

---
+++ C/Bronze/1330.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  short a, b;
+
+  scanf("%hd %hd", &a, &b);
+
+  if (a > b) {
+    printf(">");
+  } else if (a < b) {
+    printf("<");
+  } else
+    printf("==");
+}
